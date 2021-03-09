23324659b832e016af28c4ad6725d357d2f6f414
Kevin <kevi4196@stud.kea.dk>
2021-03-09 18:40:03 +0100

    Initial commit

---
+++ src/main/java/project/demo/MadspilGruppeProjektApplication.java
@@ -0,0 +1,13 @@
+package project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadspilGruppeProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadspilGruppeProjektApplication.class, args);
+    }
+
+}
